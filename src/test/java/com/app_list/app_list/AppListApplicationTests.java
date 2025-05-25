90908acf568864e23b0ee8ede1c66cabe60ecfe0
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-05-25 17:55:40 -0300

    Criação do projeto Spring Boot

---
+++ src/test/java/com/app_list/app_list/AppListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_list.app_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
